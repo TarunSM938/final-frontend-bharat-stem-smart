7bb48ba89a4b11f76e39b9805b9c374243a0ab1a
TarunSM938 <tarunsm123@gmail.com>
2026-04-02 16:30:39 +0530

    added backend code

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["developers.google.com"],
+    domains: ["developers.google.com", "127.0.0.1", "localhost"],
   },
 };
 
